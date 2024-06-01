2f0c2957b414e57376c67f4b78250f3f578c6ce7
zzzzzzc <0630peter@gmail.com>
2024-06-01 11:40:40 +0800

    username color

--- iosFinall/ViewController.swift
+++ iosFinall/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        username.layer.borderWidth = 1
+        username.layer.borderColor = UIColor(named:"9D9C9C")?.cgColor
+        username.layer.cornerRadius = 12
+        
+        
+        
     }
 
 

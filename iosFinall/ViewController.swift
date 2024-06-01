12ec6b186fa0e88039797081aee2a46197998301
zzzzzzc <0630peter@gmail.com>
2024-06-01 12:18:36 +0800

    login

--- iosFinall/ViewController.swift
+++ iosFinall/ViewController.swift
@@ -11,16 +11,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var username: UITextField!
     
+    @IBOutlet weak var password: UITextField!
     
+    @IBOutlet weak var loginclick: UIButton!
     
+    @IBAction func loginclick(_ sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        username.layer.borderWidth = 1
+        username.layer.borderWidth = 0.5
         username.layer.borderColor = UIColor(named:"9D9C9C")?.cgColor
         username.layer.cornerRadius = 12
-        
-        
+        username.frame.size.height = 45
+        //password
+        password.layer.borderWidth = 0.5
+        password.layer.borderColor = UIColor(named:"9D9C9C")?.cgColor
+        password.layer.cornerRadius = 12
+        password.frame.size.height = 45
+        //login button
+        loginclick.layer.cornerRadius = 12
         
     }
 
